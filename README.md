2a6f55285c669309ed831b08510a868ce22cb143
Joe Choe <joechoe1989@gmail.com>
2022-04-05 22:20:12 -0700

    updated HTML, JS, CSS files

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# Web-APIs---Code-Quiz
-Homework #4
+**Homework #4** -  Web APIs: Code Quiz
+
+*Description*
+  For this week's homework assignment, the task was to create a timed coding quiz with multiple-choice questions. This app will run in the browser and will feature dynamically updated HTML and CSS powered by Javascript code in addition to have a clean, polished, and responsive user interface.
+
+
+*Rationale*
+  At some point in the journey to become a full-stack web developer, it will be likely to be asked to complete a coding assessment—perhaps as part of an interview process. A typical coding assessment includes both multiple-choice questions and interactive coding challenges.
+
+
+*Objectives*
+* Create a timed quiz that can store high scores using Javascript fundamentals
+* When the start button is clicked, a timer starts and am presented with the first question
+* When a question is answered, the user is presented with a new question
+* If the question is answered incorrectly, time will be subtracted from the timer
+* When all questions are answered OR the timer reaches 0, the game is over
+* Once the game is over, the user can save their initials and score before resetting and starting over
+* Game can store high scores so that progress can be compared
+
+
+*Requirements*
+**Screenshot**
+**Link to deployed application**
+URL of deployed application:
+URL of Github repository:
--- script.js
+++ script.js
@@ -1,47 +1,40 @@
 // setting variables for quiz questions and answers
-var questions = [
+var questionBank = [
     {
         title: "Who invented Bitcoin?",
         choices: ["David Schwartz", "the Winklevoss twins", "Craig Wright", "Satoshi Nakamoto"],
-        answer: "Satoshi Nakamoto"
+        answer: 3,
     },
     {
         title: "What year was Bitcoin invented?",
         choices: ["2000", "2005", "2009", "2013"],
-        answer: "2009"
+        answer: 2,
     },
     {
         title: "Which type of network consensus protocol does the Bitcoin blockchain use?",
         choices: ["proof-of-stake", "proof-of-work", "proof-of-space", "proof-of-elapsed-time"],
-        answer: "proof-of-work"
+        answer: 2,
     },
     {
         title: "How many Bitcoin tokens will ever be created?",
         choices: ["1 million", "8 million", "15 million", "21 million"],
-        answer: "21 million"
+        answer: 3,
     },
     {
         title: "Roughly how many different cryptocurrencies are there in existence today?",
         choices: ["100", "500", "5000", "18000"],
-        answer: "18000"
+        answer: 3,
     },
     {
         title: "Which property does Bitcoin NOT possess?",
         choices: ["fungible", "decentralized", "portable", "freezable"],
-        answer: "freezable"
+        answer: 3,
     },
 ];
 
-
-var result = "";
-var initials = "";
 var score = 0;
-var seconds = 76;
-var currentQuestionIndex = 0
-
-
-// creating variables for elements
-var createlist = document.createElement("ul");
+var seconds = 75;
+var userInitial = "";
 
 var startButtonEl = document.querySelector(".startButton");
 var infoEl = document.querySelector(".info");
@@ -50,57 +43,96 @@ var countDownEl = document.querySelector("#time");
 var questionnaireEl = document.querySelector("#questionnaire");
 var choicesEl = document.querySelector("choices");
 var resultsEl = document.querySelector("#results");
-var finalScoreEl = document.querySelector("#finalScore");
 var submitButtonEl = document.querySelector ("#submitButton");
+var highScoresEl = document.querySelector ("#highScores");
+var initialContainer = document.querySelector("#initials");
+var finalScoreEl = document.querySelector("#finalScore");
+var initialScoreStorage = JSON.parse(localStorage.getItem("savedInitials")) || [];
 
 // function to start quiz once start button is clicked and start timer, hide info/intro section
-
-
 if (startButtonEl) {
     startButtonEl.addEventListener("click", function(event) {
-    //   event.preventDefault();
+      event.preventDefault();
       infoEl.setAttribute("class", "hide");
-      questionnaireEl.setAttribute("class", "");
-      resultsEl.setAttribute("class", "hide");
-      countDownTimer();
-      showQuestions();
+      startCountdown();
+      showQ(0);
     });
 }
 
-// function for timer
-function countDownTimer() {
-    var timerInterval = setInterval(function() {
+// creating div, ul, li elements and appending to HTML
+function showQ(idx) {
+    if (questionnaireEl.firstChild) {
+        questionnaireEl.removeChild(questionnaireEl.firstChild);
+    }
+    if (idx >= questionBank.length) {
+        displayRes();
+    } else {
+        var currQ = questionBank[idx];
+        var currQDiv = document.createElement("div");
+        var currQText = document.createTextNode(currQ.title);
+        var currCul = document.createElement("ul");
+    
+        for (let i = 0; i < currQ.choices.length; i++) {
+            var currCli = document.createElement("li");
+            var currCText = document.createTextNode(currQ.choices[i]);
+            currCli.appendChild(currCText);
+            currCli.onclick = function() {
+                console.log(i);
+                console.log(currQ.answer);
+                if (i === currQ.answer) {
+                    score++;
+                    alert("You are correct!");
+                } else {
+                    seconds -=10;
+                    alert("You are incorrect! 10 seconds have been deducted from timer.");
+                }
+                showQ(idx + 1);
+            }
+            currCul.appendChild(currCli);
+        }
+    
+        currQDiv.appendChild(currQText);
+        currQDiv.appendChild(currCul);
+    
+        questionnaireEl.append(currQDiv);
+    }
+}
+
+function displayRes() {
+    if (questionnaireEl.firstChild) {
+        questionnaireEl.removeChild(questionnaireEl.firstChild);
+    }
+    resultsEl.hidden = false;
+    finalScoreEl.innerHTML = score + " out of 6 correct";
+}
+
+// timer countdown function
+function startCountdown() {
+    countDownEl.textContent = seconds + " seconds remaining";
+    var countDown = setInterval(function() {
         seconds--;
-        countDownEl.textContent = "Time Remaining: " + seconds + " seconds"
+        countDownEl.textContent = seconds + " seconds remaining";
         if (seconds <= 0) {
-            clearInterval(timerInterval);
-            // function to end game
+            clearInterval(countDown);
+            displayRes();
         }
     }, 1000);
 }
 
-function showQuestions() {
-    var currentQuestion = questions[currentQuestionIndex];
-    var titleEl = document.getElementById("question-title")
-    titleEl.textContent = currentQuestion.title;
-    choicesEl.innerHTML = "";
-}
-
-// function displayResult() {
-//     resultsEl.removeAttribute("class", "hide");
-//     seconds = 0;
-//     finalScoreEl.textContent = score + "out of 6 questions correct!";
-//     finalsubmit();
-
-// }
-
-// function finalsubmit() {
-//     submitButtonEl.addEventListener("click", function(event) {
-//         event.preventDefault();
-//         user
-//     }
-//     )
-// }
-
-
-// if there's time, create function to set button to animate upon clicking the right answer
+// local storage function after user submits initials
+submitButton.addEventListener("click", function(event) {
+    var initials = document.querySelector("#initials").value;
+    var finalScore =  score
+    
+    if (initials === "") {
+        alert("This field cannot be blank", "error")
+    } else {
+        var playerData = {
+            initial:initials, 
+            score:finalScore
+        }
+        initialScoreStorage.push(playerData)
+        localStorage.setItem("savedInitials", JSON.stringify(initialScoreStorage))
+        window.location.href = "highscores.html"
+    }
+})
